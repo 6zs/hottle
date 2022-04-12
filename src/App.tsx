e89a7cdc4b8ab9e85b2dcfb578eac98fdc46b77f
6zs <49460548+6zs@users.noreply.github.com>
2022-04-11 20:51:29 -0700

    fix calendar not clearing url params

--- src/App.tsx
+++ src/App.tsx
@@ -163,7 +163,7 @@ function App() {
         maxDetail={"month"}
         onClickDay={(value: Date, event: any) => {
           if (value >= minDate && value <= maxDate) {
-            window.location.replace(window.location + "?x="+(1 + dateToNumber(value) - day1Number));
+            window.location.replace(window.location.origin + "?x="+(1 + dateToNumber(value) - day1Number));
           }
         }}
         formatDay={(locale: string, date: Date) => calendarFormatDay(locale, date)}
